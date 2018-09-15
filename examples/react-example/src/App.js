c51675a7af151a067e703119ef0dd727c9f4195d
jisatm7 <Tyman703>
2018-09-15 12:12:48 -0500

    Added material-ui to help template out the page and added a basic design for the scenarios

--- examples/react-example/src/App.js
+++ examples/react-example/src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import CardHeader from "@material-ui/core/CardHeader";
 
 class App extends Component {
   render() {
@@ -8,11 +11,25 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">React Example for Tundra-Fetch</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="App-intro">
+          <p>
+            The scenarios below demonstrate the core functionality of Tundra in a fetch-based React app
+            using <a href="https://reqres.in">reqres.in</a> as the backend REST api.
+          </p>
+        </div>
+        <div className="Container">
+          <Card className="Scenario-Card">
+            <CardHeader
+              title="Scenario 1"
+              subheader="Scenario description goes here"
+            />
+            <CardContent>
+              Scenario Details
+            </CardContent>
+          </Card>
+        </div>
       </div>
     );
   }
